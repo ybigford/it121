9e7e16e2952d77a8f14a1b1876c1edfa7aac6b09
Yapa Bigford <110362211+ybigford@users.noreply.github.com>
2024-01-01 22:09:11 -0800

    Update script.js

--- week1/script.js
+++ week1/script.js
@@ -1,1 +1,4 @@
-
+let firstName = "Yapa";
+let lastName = "Bigford";
+let yearOfStudy = "2 years";
+let goal = "My goal is to graduate and pursue my higher degree. I would like to become a professional software developer and transfer to IT department at Costco. I love coding and want to make it my career.";
